5c0245b8bace800a8a0d92017c365c56d547c4bc
RRG96 <rodrorx@gmail.com>
2020-09-21 12:51:05 -0500

    .

--- log-block.pl
+++ log-block.pl
@@ -2,9 +2,8 @@ use Proc::Daemon;
  
 $daemon = Proc::Daemon->new(
     work_dir => '/etc/log-block',
-    .....
 );
-if($ARGV[$#ARGV eq "start"){
+if($ARGV[$#ARGV] eq "start"){
     $Kid_1_PID = $daemon->Init( { 
                     exec_command => 'perl /etc/log-block/proyecto.pl start',
                  } );
